802145150c3357746b5448bfafb94bb92043b0b9
sml <sylvain.m@gmail.com>
2014-03-16 23:35:08 +0100

    Update download URL

--- Js/GSExtended.user.js
+++ Js/GSExtended.user.js
@@ -3,6 +3,8 @@
 // @name        Grooveshark Extended
 // @namespace   GSX
 // @description Enhance Grooveshark Broadcast functionnality
+// @downloadURL	https://github.com/Ramouch0/GSExtended/raw/master/Js/GSExtended.user.js
+// @updateURL	https://github.com/Ramouch0/GSExtended/raw/master/Js/GSExtended.meta.js
 // @include     http://grooveshark.com/*
 // @version     1.0.0
 // @run-at document-end
