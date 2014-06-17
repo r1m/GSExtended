89c6e42fa05361369adbf28fd629fd97f4ec6d57
Ram <ramouch0@hotmail.com>
2014-06-17 14:56:01 +0200

    reduce enlarged page width by 10px

--- Js/GSExtended.user.js
+++ Js/GSExtended.user.js
@@ -6,7 +6,7 @@
 // @downloadURL	https://github.com/Ramouch0/GSExtended/raw/master/Js/GSExtended.user.js
 // @updateURL	https://github.com/Ramouch0/GSExtended/raw/master/Js/GSExtended.user.js
 // @include     http://grooveshark.com/*
-// @version     1.5.1
+// @version     1.5.2
 // @run-at document-end
 // @grant  none 
 // ==/UserScript==
@@ -418,7 +418,7 @@ GSX = {
     },
     enlargeChatbox: function () {
         GSXTool.addStyle('.bc-chat-messages-container {background-color: #fff;}.chat-activity.chat-message .inner{width:320px !important;}.chat-activity.chat-info .inner{width:270px !important;}.bc-chat-container {width: 400px !important;}.bc-chat-form-container .bc-chat-input {width: 282px !important;}\
-		#page, #page-nav, #page-header .inner img {width:1120px !important;} #page-header .inner .song-data-container {width:946px !important;} #page-content, #column1.full{width:1060px;} #column2{width:400px}');
+		#page, #page-nav, #page-header .inner img {width:1110px !important;} #page-header .inner .song-data-container {width:936px !important;} #page-content, #column1.full{width:1050px;} #column2{width:390px}');
     },
     removeSharebox: function () {
         GSXTool.addStyle('#bc-share{display:none;}.bc-chat-messages-container {top:0px!important;}');
