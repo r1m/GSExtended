084c4bcbee867f7239c8492db1d7ba071fc5955c
Michael Writhe <michael@writhem.com>
2014-08-29 10:35:42 -0600

    added Transparent White as a theme option
    Very similar to the Transparent Black but this makes some readability improvements based on several of the broadcasts backgrounds not working well with grey text. Also shows the VIPs in a nice subtle way (shape/outline of the avatar)

--- src/GSExtended.user.js
+++ src/GSExtended.user.js
@@ -20,6 +20,7 @@ dependencies = {
     theme: {
         'default': 'https://ramouch0.github.io/GSExtended/src/css/gsx_theme_default.css',
         'oldGSX': 'https://ramouch0.github.io/GSExtended/src/css/gsx_theme_old.css',
+        'WritheM Transparent White': 'https://userstyles.org/styles/104769.css',
         'Mullins Transparent Black': 'https://userstyles.org/styles/102624.css?ik-gs-ex=ik-2&ik-gs-fr=ik-2&ik-gs-ch=ik-2&ik-gs-se=ik-2&ik-wide-gs=ik-2',
         'Mullins Metro Black': 'https://userstyles.org/styles/103472.css?ik-gs-x2=ik-2&ik-gs-fr=ik-2&ik-gs-ch=ik-2&ik-gs-se=ik-2&ik-he-op=ik-2',
         'none': false
